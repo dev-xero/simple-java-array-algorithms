1efed6a81bdaf0fc75a478871bde59f19365f0a0
Xero kun <elo48858@gmail.com>
2023-05-17 21:51:51 +0100

    `FEAT` Implemented method to compute max value in an array

---
+++ src/SimpleArrayAlgorithms.java
@@ -0,0 +1,27 @@
+public class SimpleArrayAlgorithms {
+
+    public static double findMax(double[] a) {
+        double max = a[0];
+
+        for (double i : a) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        double[] a = new double[5];
+
+        a[0] = 5.0;
+        a[1] = 4.4;
+        a[2] = 9.8;
+        a[3] = 9.6;
+        a[4] = 10.5;
+
+        System.out.println(findMax(a));
+
+    }
+}
